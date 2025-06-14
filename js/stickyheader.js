f93eedc52202deec0225cc2901717ad42917274d
tiepvd90 <tiepvd90@gmail.com>
2025-06-14 16:29:59 +0700

    Create stickyheader.js

---
+++ js/stickyheader.js
@@ -0,0 +1,6 @@
+<script>
+  function toggleCheckoutPopup(show) {
+    const popup = document.getElementById("checkoutPopup");
+    if (popup) popup.style.display = show ? "flex" : "none";
+  }
+</script>
