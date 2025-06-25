cd3a576e78d85224c7ed62d05e721129c73750e2
tiepvd90 <tiepvd90@gmail.com>
2025-06-25 22:38:32 +0700

    Update freeflow.js

--- js/freeflow.js
+++ js/freeflow.js
@@ -97,7 +97,7 @@ function renderInitialAndLoadRest() {
   setupAutoplayObserver();
 
   setTimeout(() => {
-    const remaining = freeflowData.slice(4, 100);
+    const remaining = freeflowData.slice(4);
     remaining.forEach(item => renderFeedItem(item, container));
     itemsLoaded = freeflowData.length;
     setupAutoplayObserver();
