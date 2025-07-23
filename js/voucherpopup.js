17bbf4f866b0230039eaf42b7d2e24a788f7997a
tiepvd90 <tiepvd90@gmail.com>
2025-07-24 03:46:15 +0700

    Update voucherpopup.js

--- js/voucherpopup.js
+++ js/voucherpopup.js
@@ -15,7 +15,7 @@ const simpleVoucherMap = {
 
 const allowedPages = [
   "ysandal5568", "ysandalbn68", "firstpickleball",
-  "secpickleball", "teflon", "gen4","collection", "pickleball-airforce"
+  "secpickleball", "teflon","phantom", "gen4","collection", "pickleball-airforce"
 ];
 
 function showVoucherPopup(refCode, amount) {
