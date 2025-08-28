ff7762731c04106e159a479b031edbcaeca94d52
tiepvd90 <tiepvd90@gmail.com>
2025-08-29 01:49:57 +0700

    Update slideshow-2p.js

--- js/slideshow-2p.js
+++ js/slideshow-2p.js
@@ -6,7 +6,7 @@
   const ORIGIN = location.origin.replace("http://", "https://");
 
   const IMAGE_CONFIG = {
-    zen: { count: 10, format: "jpg" },
+    zen: { count: 6, format: "jpg" },
     girl: { count: 6, format: "jpg" },
     // thêm cấu hình khác nếu cần
   };
